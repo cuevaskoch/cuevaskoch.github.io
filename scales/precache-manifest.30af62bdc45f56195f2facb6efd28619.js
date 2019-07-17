481983cfa69e92d0f79d6eb8320fc927d752fe7e
Jeff Cuevas-Koch <jeff@cuevaskoch.com>
2019-07-17 07:58:12 -0700

    Add scale trainer
    Signed-off-by: Jeff Cuevas-Koch <jeff@cuevaskoch.com>

---
+++ scales/precache-manifest.30af62bdc45f56195f2facb6efd28619.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "247ddec102ec46e14b7aa467fc8ef0a4",
+    "url": "/scales/index.html"
+  },
+  {
+    "revision": "d1abfb6981cda5d172a8",
+    "url": "/scales/static/css/main.a19d56ef.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/scales/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "d1abfb6981cda5d172a8",
+    "url": "/scales/static/js/main.feae639f.chunk.js"
+  },
+  {
+    "revision": "b4c1fae5914ba72de7d4",
+    "url": "/scales/static/js/runtime~main.b1bebba8.js"
+  }
+]);
